f95e6b0028ac5c38c0743ebba5ec3b2e9785a4bc
haseeb-ali-dev <h45.busy@gmail.com>
2022-10-04 06:07:42 -0700

    react router setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Navbar from 'components/Navbar'
 import Order from 'containers/order'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => (
-  <div>
+  <Router>
     <Navbar />
-    <Order />
-  </div>
+    <Routes>
+      <Route path='/' element={<Order />} />
+      <Route path='/order' element={<Order />} />
+      <Route path='/person' element={<Order />} />
+    </Routes>
+  </Router>
 )
 
 export default App
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import Toggler from 'assets/images/toggler.png'
 
@@ -9,9 +10,9 @@ const index = () => {
   const [navClass, iconClass] = [`topnav ${responsive && 'responsive'}`, `icon ${responsive && 'active'}`]
   return (
     <div className={navClass} id='myTopnav'>
-      <a href='#home' className='active'>Expense App</a>
-      <a href='#news'>Orders</a>
-      <a href='#contact'>Create New</a>
+      <Link to='/' className='active'>Expense App</Link>
+      <Link to='/order'>Orders</Link>
+      <Link to='/'>Create New</Link>
       <button type='button' className={iconClass} onClick={() => setResponsive(!responsive)}>
         <img src={Toggler} alt='toggler' />
       </button>
