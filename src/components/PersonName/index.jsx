14156368a35df28e2455501e47f5af071422593d
haseeb-ali-dev <h45.busy@gmail.com>
2022-10-24 06:18:21 -0700

    person list from users

--- src/components/PersonName/index.jsx
+++ src/components/PersonName/index.jsx
@@ -1,19 +1,40 @@
+import { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
+import SelectBox from 'components/SelectBox'
+
 import { ADD_PERSON_NAME } from 'store/person'
+import { getUsers } from 'api/auth'
 
 const personName = () => {
   const dispatch = useDispatch()
-  const { name } = useSelector(state => state.person)
+  const [users, setUsers] = useState()
+  const { name: nameOfPerson } = useSelector(state => state.person)
+  const persons = useSelector(state => state.personList)
+
   const addPerson = (e) => {
     const personname = e.target.value
-    dispatch(ADD_PERSON_NAME({ name: personname.length === 0 ? 'Auto Generated Person 1' : personname }))
+    dispatch(ADD_PERSON_NAME({ name: personname }))
   }
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const fetchedUsers = await getUsers()
+      const updatedUsers = fetchedUsers.map(({ name }) => ({
+        name,
+        value: name,
+        disabled: persons.some(person => (person.name === name)),
+      }))
+      setUsers(updatedUsers)
+    }
+    fetchUsers()
+  }, [])
+
   return (
     <>
-      <label htmlFor='inputPassword' className='col-sm-2 col-form-label'>Person Name</label>
+      <label htmlFor='inputPassword' className='col-sm-2 col-form-label'>Select Person</label>
       <div className='col-sm-4'>
-        <input type='text' className='form-control' placeholder='Enter Person Name' onChange={addPerson} value={name} />
+        <SelectBox name='persons' options={users} onChange={addPerson} value={nameOfPerson} />
       </div>
     </>
   )
