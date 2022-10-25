ef7b79251026427c9dba484394f9d1108cfbd93a
haseeb-ali-dev <h45.busy@gmail.com>
2022-10-25 09:03:29 -0700

    order details in a modal

---
+++ src/components/OrderListItem/index.jsx
@@ -0,0 +1,58 @@
+import { useDispatch } from 'react-redux'
+
+import { SHOW_MODAL } from 'store/modal'
+
+import ListItem from 'components/ListItem'
+
+const orderListItem = ({ order, openModal = null, showDetails = false }) => {
+  const dispatch = useDispatch()
+  const viewOrder = (detailedOrder) => {
+    openModal(detailedOrder)
+    dispatch(SHOW_MODAL())
+  }
+
+  return (
+    <div className='border border-1 rounded-2 m-1 p-2'>
+      <div className='d-flex w-100 justify-content-between'>
+        <h5 className='mb-1'>{order.resturant}</h5>
+      </div>
+      <div className='mb-1 d-inline-flex flex-wrap'>
+        <div className='mt-2 mx-1 border py-1 px-2 d-flex flex-column bg-light'>
+          <p className='fs-5 p-1'>Summary</p>
+          <p> <span className='text-muted'>Grand: PKR/-</span> {order.grand}</p>
+          <p> <span className='text-muted'>Tip: PKR/-</span> {order.grand}</p>
+          <p> <span className='text-muted'>Tax: PKR/-</span> {order.grand}</p>
+          <p> <span className='text-muted'>Delivery: PKR/-</span> {order.grand}</p>
+        </div>
+        {showDetails && order.persons.map((person, idx) => (
+          <div className='mt-2 mx-1 border py-1 px-2 d-flex flex-column bg-light flex-wrap' key={`person-${idx.toString()}`}>
+            <div className='fs-5 p-2'>{person.name}</div>
+            <div><small className='text-muted'>Total: PKR/- </small> {person.total}</div>
+            <div><small className='text-muted'>Paid: PKR/- </small> {person.paid}</div>
+            <div><small className='text-muted'>Receiable: PKR/- </small><span className='text-success'>{person.receivable}</span></div>
+            <div><small className='text-muted'>Payable: PKR/- </small>
+              <span className='text-danger'>
+                {person.payable}
+                {(person.payable !== 0 && order.receivers.length !== 0)
+                  ? ` to ${order.receivers.map(r => ` ${r.name} `)}`
+                  : ''}
+              </span>
+            </div>
+            <div className='fs-5 mt-2'>Items:</div>
+            <div>
+              {person.items.map(item => <ListItem item={item} key={item.name} />)}
+            </div>
+          </div>
+        ))}
+      </div>
+      {!showDetails
+        && (
+          <div className='w-100 mt-1 text-end p-1'>
+            <button type='button' className='btn btn-sm btn-outline-success rounded-pill' onClick={() => viewOrder(order)}>Show Details</button>
+          </div>
+        )}
+    </div>
+  )
+}
+
+export default orderListItem
--- src/containers/ordersList/index.jsx
+++ src/containers/ordersList/index.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable no-nested-ternary */
 import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
 
 import { getOrders } from 'api/order'
 
-import ListItem from 'components/ListItem'
+import Modal from 'components/Modal'
+import OrderListItem from 'components/OrderListItem'
 
 const ordersList = () => {
+  const { show } = useSelector(state => state.modal)
   const [orders, setOrders] = useState([])
   const [fetched, setFetched] = useState(false)
+  const [modalOrder, setModalOrder] = useState()
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -19,42 +23,18 @@ const ordersList = () => {
   }, [])
 
   const ordersListing = orders.map(order => (
-    <div className='list-group-item list-group-item-action' key={order.id}>
-      <div className='d-flex w-100 justify-content-between'>
-        <h5 className='mb-1'>{order.resturant}</h5>
-      </div>
-      <div className='mb-1 d-inline-flex flex-wrap'>
-        <div className='mt-2 mx-1 border py-1 px-2 d-flex flex-column bg-light'>
-          <p className='fs-4 p-1'>Summary</p>
-          <p> <span className='text-muted'>Grand: PKR/-</span> {order.grand}</p>
-          <p> <span className='text-muted'>Tip: PKR/-</span> {order.grand}</p>
-          <p> <span className='text-muted'>Tax: PKR/-</span> {order.grand}</p>
-          <p> <span className='text-muted'>Delivery: PKR/-</span> {order.grand}</p>
-        </div>
-        {order.persons.map((person, idx) => (
-          <div className='mt-2 mx-1 border py-1 px-2 d-flex flex-column bg-light flex-wrap' key={`person-${idx.toString()}`}>
-            <div className='fs-5 p-2'>{person.name}</div>
-            <div><small className='text-muted'>Total: PKR/- </small> {person.total}</div>
-            <div><small className='text-muted'>Paid: PKR/- </small> {person.paid}</div>
-            <div><small className='text-muted'>Receiable: PKR/- </small><span className='text-success'>{person.receivable}</span></div>
-            <div><small className='text-muted'>Payable: PKR/- </small>
-              <span className='text-danger'>
-                {person.payable}
-                {(person.payable !== 0 && order.receivers.length !== 0)
-                  ? ` to ${order.receivers.map(r => ` ${r.name} `)}`
-                  : ''}
-              </span>
-            </div>
-            <div className='fs-5 mt-2'>Items:</div>
-            <div>
-              {person.items.map(item => <ListItem item={item} key={item.name} />)}
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <OrderListItem
+      order={order}
+      openModal={setModalOrder}
+      key={order.id}
+    />
   ))
-  const displayOrders = <div className='list-group p-2'>{ordersListing}</div>
+  const displayOrders = (
+    <div className='d-inline-flex flex-wrap p-2'>
+      {ordersListing}
+      {show && <Modal html={<OrderListItem order={modalOrder} showDetails />} />}
+    </div>
+  )
   const loading = (
     <div className='d-flex align-items-center justify-content-center'>
       <div className='spinner-grow text-primary' role='status'>
